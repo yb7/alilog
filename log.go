3c54a8deec72910d216b48dcbbe88220151364bc
wangyb <wyb7@qq.com>
2019-09-30 14:49:12 +0800

    f

--- log.go
+++ log.go
@@ -1,41 +1,45 @@
 package alilog
 
 import (
-  "fmt"
-  "log"
-  "strings"
-  "time"
-  "sync"
-  "errors"
-  "os"
+	"errors"
+	"fmt"
+	"log"
+	"os"
+	"path/filepath"
+	"runtime"
+	"strconv"
+	"strings"
+	"sync"
+	"time"
 
-  sls "github.com/aliyun/aliyun-log-go-sdk"
+	sls "github.com/aliyun/aliyun-log-go-sdk"
 )
 
 type SLog struct {
-  projectName  string
-  logStoreName string
-  //project      *sls.LogProject
-  //logStore     *sls.LogStore
-  params       map[string]string
+	projectName  string
+	logStoreName string
+	//project      *sls.LogProject
+	//logStore     *sls.LogStore
+	params map[string]string
 }
+
 func getLogStore(projectName, logStoreName string) *sls.LogStore {
-  logKey := logStoreKey(projectName, logStoreName)
-  data, ok := logCache.Load(logKey)
-  if !ok {
-    return nil
-  }
-  return data.(*slsLogData).logStore
+	logKey := logStoreKey(projectName, logStoreName)
+	data, ok := logCache.Load(logKey)
+	if !ok {
+		return nil
+	}
+	return data.(*slsLogData).logStore
 }
 
 type slsLogData struct {
-  //once         *sync.Once
-  project      *sls.LogProject
-  logStore     *sls.LogStore
+	//once         *sync.Once
+	project  *sls.LogProject
+	logStore *sls.LogStore
 }
 
 func logStoreKey(projectName, logStoreName string) string {
-  return fmt.Sprintf("%s-%s", projectName, logStoreName)
+	return fmt.Sprintf("%s-%s", projectName, logStoreName)
 }
 
 var logCache sync.Map
@@ -43,154 +47,193 @@ var logCache sync.Map
 //var mutex = &sync.Mutex{}
 
 func initSlsLogData(projectName, logStoreName string) {
-  logKey := logStoreKey(projectName, logStoreName)
-  //const MaxRetry = 5
-  doOnce(logKey, func() error {
-    logProject := &sls.LogProject{
-      Name:            projectName,
-      Endpoint:        slsConfig.EndPoint,
-      AccessKeyID:     slsConfig.AccessKeyID,
-      AccessKeySecret: slsConfig.AccessKeySecret,
-    }
+	logKey := logStoreKey(projectName, logStoreName)
+	//const MaxRetry = 5
+	doOnce(logKey, func() error {
+		logProject := &sls.LogProject{
+			Name:            projectName,
+			Endpoint:        slsConfig.EndPoint,
+			AccessKeyID:     slsConfig.AccessKeyID,
+			AccessKeySecret: slsConfig.AccessKeySecret,
+		}
 
-    fmt.Printf("[SLS] create log store to: Project[%s], LogStore[%s], Endpoint[%s]\n", projectName, logStoreName, slsConfig.EndPoint)
+		fmt.Printf("[SLS] create log store to: Project[%s], LogStore[%s], Endpoint[%s]\n", projectName, logStoreName, slsConfig.EndPoint)
 
-    //var retry_times int
-    for retry_times := 0; retry_times < 5 && len(slsConfig.EndPoint) > 0; retry_times++ {
-      logstore, err := logProject.GetLogStore(logStoreName)
-      if err != nil {
-        fmt.Printf("GetLogStore fail, retry:%d, err:%v\n", retry_times, err)
-        if strings.Contains(err.Error(), sls.PROJECT_NOT_EXIST) {
-          return err
-        } else if strings.Contains(err.Error(), sls.LOGSTORE_NOT_EXIST) {
-          err = logProject.CreateLogStore(logStoreName, 30, 2, true, 100)
-          if err != nil {
-            fmt.Printf("CreateLogStore fail, err: %s\n", err.Error())
-          } else {
-            fmt.Println("CreateLogStore success")
-          }
-        }
-      } else {
-        fmt.Printf("GetLogStore success, retry:%d, name: %s, ttl: %d, shardCount: %d, createTime: %d, lastModifyTime: %d\n",
-          retry_times, logstore.Name, logstore.TTL, logstore.ShardCount, logstore.CreateTime, logstore.LastModifyTime)
-        logCache.Store(logKey, &slsLogData{
-          logProject, logstore,
-        })
-        return nil
-      }
-      time.Sleep(200 * time.Millisecond)
-    }
-    return errors.New("[SLS] exceed max retry, create log store failed")
-    //slsLog.logStore = logstore
-  })
+		//var retry_times int
+		for retry_times := 0; retry_times < 5 && len(slsConfig.EndPoint) > 0; retry_times++ {
+			logstore, err := logProject.GetLogStore(logStoreName)
+			if err != nil {
+				fmt.Printf("GetLogStore fail, retry:%d, err:%v\n", retry_times, err)
+				if strings.Contains(err.Error(), sls.PROJECT_NOT_EXIST) {
+					return err
+				} else if strings.Contains(err.Error(), sls.LOGSTORE_NOT_EXIST) {
+					err = logProject.CreateLogStore(logStoreName, 30, 2, true, 100)
+					if err != nil {
+						fmt.Printf("CreateLogStore fail, err: %s\n", err.Error())
+					} else {
+						fmt.Println("CreateLogStore success")
+					}
+				}
+			} else {
+				fmt.Printf("GetLogStore success, retry:%d, name: %s, ttl: %d, shardCount: %d, createTime: %d, lastModifyTime: %d\n",
+					retry_times, logstore.Name, logstore.TTL, logstore.ShardCount, logstore.CreateTime, logstore.LastModifyTime)
+				logCache.Store(logKey, &slsLogData{
+					logProject, logstore,
+				})
+				return nil
+			}
+			time.Sleep(200 * time.Millisecond)
+		}
+		return errors.New("[SLS] exceed max retry, create log store failed")
+		//slsLog.logStore = logstore
+	})
 }
 
 func New(projectName, logStoreName string) *SLog {
-  slsLog := &SLog{
-    projectName:  projectName,
-    logStoreName: logStoreName,
-  }
-  go initSlsLogData(projectName, logStoreName)
+	slsLog := &SLog{
+		projectName:  projectName,
+		logStoreName: logStoreName,
+	}
+	go initSlsLogData(projectName, logStoreName)
 
-  return slsLog
-  // return &SLog{
-  // 	project:  project,
-  // 	logStore: logStore,
-  // }
+	return slsLog
+	// return &SLog{
+	// 	project:  project,
+	// 	logStore: logStore,
+	// }
 }
 func (l *SLog) With(k, v string) *SLog {
-  params := make(map[string]string)
-  for k, v := range l.params {
-    params[k] = v
-  }
-  params[k] = v
-  return &SLog{
-    projectName:  l.projectName,
-    logStoreName: l.logStoreName,
-    //project:      l.project,
-    //logStore:     l.logStore,
-    params:       params,
-  }
+	params := make(map[string]string)
+	for k, v := range l.params {
+		params[k] = v
+	}
+	params[k] = v
+	return &SLog{
+		projectName:  l.projectName,
+		logStoreName: l.logStoreName,
+		//project:      l.project,
+		//logStore:     l.logStore,
+		params: params,
+	}
 }
 func (l *SLog) Debugf(format string, v ...interface{}) {
-  l.doLog("debug", format, v...)
+	l.doLog("debug", format, v...)
 }
 
 func (l *SLog) Infof(format string, v ...interface{}) {
-  l.doLog("info", format, v...)
+	l.doLog("info", format, v...)
 }
 
 func (l *SLog) Warnf(format string, v ...interface{}) {
-  l.doLog("warn", format, v...)
+	l.doLog("warn", format, v...)
 }
 
 func (l *SLog) Errorf(format string, v ...interface{}) error {
-  l.doLog("error", format, v...)
-  return fmt.Errorf(format, v...)
+	l.doLog("error", format, v...)
+	return fmt.Errorf(format, v...)
 }
 func (l *SLog) Error(err error) error {
-  if err != nil {
-    l.doLog("error", err.Error())
-  }
-  return err
+	if err != nil {
+		l.doLog("error", err.Error())
+	}
+	return err
 }
 
+//func caller() string {
+//  pc, _, _, _ := runtime.Caller(1)
+//  return runtime.FuncForPC(pc).Name()
+//}
+//func printCallerName() string {
+//  pc, _, _, _ := runtime.Caller(2)
+//  return runtime.FuncForPC(pc).Name()
+//}
 func (l *SLog) doLog(level string, format string, v ...interface{}) {
-  if ShouldLog(level) == false {
-    return
-  }
-  msg := fmt.Sprintf(format, v...)
-  params := make([]string, 0)
+	if ShouldLog(level) == false {
+		return
+	}
 
-  fileParam := l.params["file"]
-  if funcParam, ok := l.params["func"]; ok {
-    fileParam = fmt.Sprintf("%s[%s]", fileParam, funcParam)
-  }
+	msg := fmt.Sprintf(format, v...)
+	params := make([]string, 0)
 
-  for k, v := range l.params {
-    if k != "file" && k != "func" {
-      params = append(params, fmt.Sprintf("%s[%s]", k, v))
-    }
-  }
+	fileName, fileNameExisted := l.params["file"]
+	funcName, funcNameExisted := l.params["func"]
 
-  var stdLog *log.Logger
-  switch level {
-  case "debug":
-    stdLog = stdDebug
-  case "warn":
-    stdLog = stdWarning
-  case "error":
-    stdLog = stdError
-  default:
-    stdLog = stdInfo
-  }
+	var lineNumber = ""
 
-  msgArr := make([]string, 0, 3)
-  if len(fileParam) > 0 {
-    msgArr = append(msgArr, fileParam)
-  }
-  if len(params) > 0 {
-    msgArr = append(msgArr, strings.Join(params, ", "))
-  }
-  msgArr = append(msgArr, msg)
-  stdLog.Println(strings.Join(msgArr, " - "))
-  if len(strings.TrimSpace(os.Getenv("ALILOG_CONFIG"))) > 0 {
-    contents := map[string]string{
-      "level":   level,
-      "message": msg,
-    }
-    for k, v := range l.params {
-      contents[k] = v
-    }
-    logChan <- &logDto{
-      ProjectName:  l.projectName,
-      LogStoreName: l.logStoreName,
-      // Project:  l.project,
-      //LogStore: l.logStore,
-      Time:     time.Now(),
-      Contents: contents,
-    }
-  } else {
-    _debug("logStore is null, ignore the log\n")
-  }
+	if l.params == nil {
+		l.params = make(map[string]string)
+	}
+
+	if !fileNameExisted || !funcNameExisted {
+		pc, file, line, ok := runtime.Caller(2)
+		if ok {
+			if !fileNameExisted {
+				arr := strings.Split(filepath.ToSlash(file), "/")
+				fileName = arr[len(arr)-1]
+				if len(arr) > 1 {
+					fileName = fmt.Sprintf("%s/%s", arr[len(arr)-2], fileName)
+				}
+				l.params["file"] = fileName
+			}
+			if !funcNameExisted {
+				funcName = runtime.FuncForPC(pc).Name()
+				l.params["func"] = funcName
+			}
+			lineNumber = fmt.Sprintf(":%d", line)
+			l.params["line"] = strconv.Itoa(line)
+		}
+	}
+
+	fileParam := fmt.Sprintf("%s[%s%s]", fileName, funcName, lineNumber)
+
+	//if funcParam, ok := l.params["func"]; ok {
+	//  fileParam = fmt.Sprintf("%s[%s]", fileParam, funcParam)
+	//}
+
+	for k, v := range l.params {
+		if k != "file" && k != "func" && k != "line" {
+			params = append(params, fmt.Sprintf("%s[%s]", k, v))
+		}
+	}
+
+	var stdLog *log.Logger
+	switch level {
+	case "debug":
+		stdLog = stdDebug
+	case "warn":
+		stdLog = stdWarning
+	case "error":
+		stdLog = stdError
+	default:
+		stdLog = stdInfo
+	}
+
+	msgArr := make([]string, 0, 3)
+	if len(fileParam) > 0 {
+		msgArr = append(msgArr, fileParam)
+	}
+	if len(params) > 0 {
+		msgArr = append(msgArr, strings.Join(params, ", "))
+	}
+	msgArr = append(msgArr, msg)
+	stdLog.Println(strings.Join(msgArr, " - "))
+	if len(strings.TrimSpace(os.Getenv("ALILOG_CONFIG"))) > 0 {
+		contents := map[string]string{
+			"level":   level,
+			"message": msg,
+		}
+		for k, v := range l.params {
+			contents[k] = v
+		}
+		logChan <- &logDto{
+			ProjectName:  l.projectName,
+			LogStoreName: l.logStoreName,
+			// Project:  l.project,
+			//LogStore: l.logStore,
+			Time:     time.Now(),
+			Contents: contents,
+		}
+	} else {
+		_debug("logStore is null, ignore the log\n")
+	}
 }
